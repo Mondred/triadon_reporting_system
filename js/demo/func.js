224c190063f614f860681ed7368b12a2ea828374
Mondred <munsod.mondred@gmail.com>
2021-06-03 13:51:58 +0800

    set

--- js/demo/func.js
+++ js/demo/func.js
@@ -1,33 +1,3 @@
-Array.prototype.defineProperty('groupByProperties', {
-  value : function(properties){                       
-      // will contain grouped items
-      var result = []; 
-
-      // iterate over each item in the original array
-      this.forEach(function(item){
-          // check if the item belongs in an already created group
-          var added = result.some(function(group){
-              // check if the item belongs in this group
-              var shouldAdd = properties.every(function(prop){
-                  return (group[0][prop] === item[prop]);
-              });
-              // add item to this group if it belongs 
-              if (shouldAdd) {
-                  group.push(item);
-              }
-              // exit the loop when an item is added, continue if not
-              return shouldAdd;
-          });
-
-          // no matching group was found, so a new group needs to be created for this item
-          if (!added) {
-              result.push([item]);
-          }
-      });
-      return result;
-  }
-});
-
 let categorylist = [];
 
 function getApps(res){
@@ -160,13 +130,7 @@ const dt_ = {
   },
 }
 
-// Variable declaration for Date From and To
-var fmo = '';
-var tmo = '';
 
-let from = '';
-let to = '';
-let sd = 01;let rset = 01;
 
 
 async function _getusrs(tkn){
--- js/functions.js
+++ js/functions.js
@@ -5,5 +5,42 @@ const spnr = (id)=>{
     }
 }
 
-// CSS Loader
+// CSS Loader end
 
+Array.prototype.defineProperty('groupByProperties', {
+    value : function(properties){                       
+        // will contain grouped items
+        var result = []; 
+  
+        // iterate over each item in the original array
+        this.forEach(function(item){
+            // check if the item belongs in an already created group
+            var added = result.some(function(group){
+                // check if the item belongs in this group
+                var shouldAdd = properties.every(function(prop){
+                    return (group[0][prop] === item[prop]);
+                });
+                // add item to this group if it belongs 
+                if (shouldAdd) {
+                    group.push(item);
+                }
+                // exit the loop when an item is added, continue if not
+                return shouldAdd;
+            });
+  
+            // no matching group was found, so a new group needs to be created for this item
+            if (!added) {
+                result.push([item]);
+            }
+        });
+        return result;
+    }
+  });
+
+  // Variable declaration for Date From and To
+    var fmo = '';
+    var tmo = '';
+
+    let from = '';
+    let to = '';
+    let sd = 01;let rset = 01;
