e1c3f3e6d8642cc9629b7a29fd996cd3bdaf3399
Mondred <munsod.mondred@gmail.com>
2021-06-03 16:11:11 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -38,7 +38,7 @@ const spnr = (id)=>{
   }; */
 
   function getGroup(list,props){
-    var result = []; var score = 0;
+    var result,result2 = []; var score,score2 = 0;
   
     // iterate over each item in the original array
     list.forEach(function(item){
@@ -63,7 +63,20 @@ const spnr = (id)=>{
 
 
             if (item.type === 'app') {
+                let shouldAdd = true;
+                if (result2.length) {
+                    result2.forEach(cat => {
+                        if (item.category === cat) {
+                            shouldAdd = false;
+                        }
+                    });
+                }
                 
+                if (shouldAdd) {
+                    result2.push(item.category);
+                    score2+=item.score;
+                    //console.log(item.category, item.title);
+                }
             }
             // check if the item belongs in this group
             
@@ -75,8 +88,9 @@ const spnr = (id)=>{
 
         // no matching group was found, so a new group needs to be created for this item
     let tscore = ((score/result.length)*100)/4;
-    console.log(tscore);
-    
+    let tscore2 = ((score2/result2.length)*100)/4;
+    console.log(tscore,tscore2);
+
     return result;
   }
 
