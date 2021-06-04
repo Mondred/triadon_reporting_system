d2430b4f347a10e77235bb3b54a0c323e5419204
Mondred <munsod.mondred@gmail.com>
2021-06-04 23:40:50 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -14,13 +14,19 @@ const spnr = (id)=>{
             // check if the item belongs in an already created group
             if (item.type === 'web') {
 
+                let shouldAdd0 = true;
                 if (scorearray.length) {
                     scorearray.forEach(items=>{
                         if (item.category === items.category) {
+                            shouldAdd0 = false;
                             items.score += item.score;
                         }
                     });
                 }
+                if (shouldAdd0) {
+                    scorearray.push(item);
+                }
+
 
                 let shouldAdd = true;
                 let shouldAdd2 = true;
